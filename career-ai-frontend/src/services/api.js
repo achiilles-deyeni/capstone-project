4cc6140dda36c3c81cc03ec0c31a863d83deb13e
Achilles Deyeni <164770211+achiilles-deyeni@users.noreply.github.com>
2025-10-10 03:28:10 +0000

    folder structure update

--- career-ai-frontend/src/services/api.js
+++ career-ai-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// Automatically read from environment variable
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000", // your FastAPI backend URL
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 });
 
 export default API;
